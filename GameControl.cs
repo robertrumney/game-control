c569d9a65e2149266b3611e68089374eb0e858fc
Robert Rumney <robertrumney@gmail.com>
2023-05-01 17:07:42 +0200

    Update GameControl.cs

--- GameControl.cs
+++ GameControl.cs
@@ -1,57 +1,35 @@
 using UnityEngine;
 
-public static class GameControl : object 
+public static class GameControl
 {
-	public static bool paused=false;
-	public static bool inventory=false;
-	public static bool cutscene=false;
-	public static bool objectives=false;
-	public static bool conversation=false;
-	public static bool importantConversation=false;
-	public static bool tutorial=false;
-	public static bool gasMask=false;
-	public static bool labcoat=true;
-	public static bool inCar=false;
-	public static bool thirdPerson=false;
-	public static bool dead=false;
-	public static bool vendor=false;
-	public static bool bulletTime=false;
-	public static bool inTank=false;
-	public static string currentSlot;
+    public static bool paused, inventory, cutscene, objectives, conversation, importantConversation, tutorial, gasMask, labcoat = true;
+    public static bool inCar, thirdPerson, dead, vendor, bulletTime, inTank;
+    public static bool fauna = true, wildlife = true, elements = true, aa = true, motionBlur = true;
+    public static string currentSlot;
+    public static int level, xp;
+    public static int[] skills = new int[4];
+    public static string saveName = "SLOT_1";
 
-	public static int level;
-	public static int xp;
-	public static int[] skills=new int[4];
+    public static GameObject player()
+    {
+        return GameObject.FindWithTag("Player");
+    }
 
-	public static bool fauna=true;
-	public static bool wildlife=true;
-	public static bool elements=true;
-	public static bool aa=true;
-	public static bool motionBlur=true;
+    public static void GetCurrentSlot()
+    {
+        currentSlot = PlayerPrefs.GetString("CurrentSlot");
+    }
 
-	public static string saveName="SLOT_NEW3";
-
-	public static GameObject player()
-	{
-		GameObject x=GameObject.FindWithTag("Player");
-		return x;
-	}
-
-	public static void GetCurrentSlot()
-	{
-		currentSlot=PlayerPrefs.GetString("CurrentSlot");
-	}
-
-	public static void DebugLog()
-	{
-		Debug.Log ( "paused is "+ paused);
-		Debug.Log ( "inventory is "+inventory);
-		Debug.Log ( "cutscene is "+cutscene);
-		Debug.Log ( "objectives is "+ objectives);
-		Debug.Log ( "conversation is "+conversation);
-		Debug.Log ( "tutorial is "+ tutorial);
-		Debug.Log ( "gasMask is " +gasMask);
-		Debug.Log ( "inCar is "+ inCar);
-		Debug.Log ( "thirdPerson is "+ thirdPerson);
-	}
+    public static void DebugLog()
+    {
+        Debug.Log("paused is " + paused);
+        Debug.Log("inventory is " + inventory);
+        Debug.Log("cutscene is " + cutscene);
+        Debug.Log("objectives is " + objectives);
+        Debug.Log("conversation is " + conversation);
+        Debug.Log("tutorial is " + tutorial);
+        Debug.Log("gasMask is " + gasMask);
+        Debug.Log("inCar is " + inCar);
+        Debug.Log("thirdPerson is " + thirdPerson);
+    }
 }
